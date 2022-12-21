e474186f6bba705a61f728955a1dea7c8e632cf6
BronzeToad <github@ajpf.me>
2022-12-21 07:43:35 -0600

    refactor

--- models/baseball_databank.py
+++ models/baseball_databank.py
@@ -6,26 +6,25 @@ import helpers.data_utilities as dataUtils
 
 from icecream import ic
 
+
 # ============================================================================ #
 
 class BaseballDataBank:
-    
     SOURCE_URL = 'https://raw.githubusercontent.com/chadwickbureau/baseballdatabank/master'
     ROOT_DIR = EnvHelper().workspace_dir
-    CONFIG = dataUtils.get_json(folder=osPath.join(ROOT_DIR, 'configs'), 
-                                 filename = 'baseball_databank')
-    
-# ============================================================================ #
+    CONFIG = dataUtils.get_json(folder=osPath.join(ROOT_DIR, 'configs'),
+                                filename='baseball_databank')
+
+    # ============================================================================ #
 
     def __init__(self, filenames: list = None):
         self.VALID_FILENAMES = None
         self.filenames = filenames
-        
-        
+
     @property
     def VALID_FILENAMES(self) -> list:
         return self._VALID_FILENAMES
-    
+
     @VALID_FILENAMES.setter
     def VALID_FILENAMES(self, val: list) -> None:
         val = []
@@ -33,12 +32,11 @@ class BaseballDataBank:
             _name = _key['fileName']
             val.append(_name)
         self._VALID_FILENAMES = val
-        
-        
+
     @property
     def filenames(self) -> list:
         return self._filenames
-    
+
     @filenames.setter
     def filenames(self, val) -> None:
         if val is None:
@@ -54,5 +52,5 @@ class BaseballDataBank:
                 print(f'Removed {len(_invalid_filenames)} invalid filenames...')
                 ic(_invalid_filenames)
         self._filenames = val
-        
+
 # ============================================================================ #
